f21f573785219e8d94b54822db6e25570f8c3ac8
Kaushal Kumar <kaushal.kumar@codinova.com>
2026-02-03 16:13:43 +0530

    adding emp

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { EmployeeService } from './employee/employee.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
